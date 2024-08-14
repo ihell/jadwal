ff4c58998b63bca66fc743bc7cd983fd615914be
ihell <ihell269@gmail.com>
2024-08-14 09:49:24 +0700

    ubah database

--- functions.js
+++ functions.js
@@ -27,15 +27,16 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export async function ambilDaftarSenin() {
-  const refDokumen = collection(db, "senin");
-  const kueri = query(refDokumen, orderBy("jam1"));
+export async function ambilDaftarJadwal() {
+  const refDokumen = collection(db, "jadwal");
+  const kueri = query(refDokumen, orderBy("urutan"));
   const cuplikanKueri = await getDocs(kueri);
 
   let hasil = [];
   cuplikanKueri.forEach((dok) => {
     hasil.push({
       id: dok.id,
+      hari: dok.data().hari,
       jam1: dok.data().jam1,
       x1: dok.data().x1,
       xi1: dok.data().xi1,
@@ -53,125 +54,40 @@ export async function ambilDaftarSenin() {
   return hasil;
 }
 
-// ambil data mapel hari selasa
-export async function ambilDaftarSelasa() {
-  const refDokumen = collection(db, "selasa");
-  const kueri = query(refDokumen, orderBy("jam1"));
-  const cuplikanKueri = await getDocs(kueri);
-
-  let hasil = [];
-  cuplikanKueri.forEach((dok) => {
-    hasil.push({
-      id: dok.id,
-      jam1: dok.data().jam1,
-      x1: dok.data().x1,
-      xi1: dok.data().xi1,
-      xii1: dok.data().xii1,
-      jam2: dok.data().jam2,
-      x2: dok.data().x2,
-      xi2: dok.data().xi2,
-      xii2: dok.data().xii2,
-      jam3: dok.data().jam3,
-      x3: dok.data().x3,
-      xi3: dok.data().xi3,
-      xii3: dok.data().xii3,
-      jam4: dok.data().jam4,
-      x4: dok.data().x4,
-      xi4: dok.data().xi4,
-      xii4: dok.data().xii4
-    });
-  });
-  return hasil;
+export function formatAngka(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
 
-// ambil data mapel hari rabu
-export async function ambilDaftarRabu() {
-  const refDokumen = collection(db, "rabu");
-  const kueri = query(refDokumen, orderBy("jam1"));
-  const cuplikanKueri = await getDocs(kueri);
-
-  let hasil = [];
-  cuplikanKueri.forEach((dok) => {
-    hasil.push({
-      id: dok.id,
-      jam1: dok.data().jam1,
-      x1: dok.data().x1,
-      xi1: dok.data().xi1,
-      xii1: dok.data().xii1,
-      jam2: dok.data().jam2,
-      x2: dok.data().x2,
-      xi2: dok.data().xi2,
-      xii2: dok.data().xii2,
-      jam3: dok.data().jam3,
-      x3: dok.data().x3,
-      xi3: dok.data().xi3,
-      xii3: dok.data().xii3,
-      jam4: dok.data().jam4,
-      x4: dok.data().x4,
-      xi4: dok.data().xi4,
-      xii4: dok.data().xii4
-    });
+export async function ubahJadwal(docId, jam1, x1, xi1, xii1,
+  jam2, x2, xi2, xii2,
+  jam3, x3, xi3, xii3
+) {
+  await updateDoc(doc(db, "jadwal", docId), {
+    jam1: jam1,
+    x1: x1,
+    xi1: xi1,
+    xii1: xii1,
+    jam2: jam2,
+    x2: x2,
+    xi2: xi2,
+    xii2: xii2,
+    jam3: jam3,
+    x3: x3,
+    xi3: xi3,
+    xii3: xii3
   });
-  return hasil;
 }
 
-// ambil data mapel hari kamis
-export async function ambilDaftarKamis() {
-  const refDokumen = collection(db, "kamis");
-  const kueri = query(refDokumen, orderBy("jam1"));
-  const cuplikanKueri = await getDocs(kueri);
+//  ambil data
+export async function ambilJadwal(docId) {
+  const docRef = await doc(db, "jadwal", docId);
+  const docSnap = await getDoc(docRef);
 
-  let hasil = [];
-  cuplikanKueri.forEach((dok) => {
-    hasil.push({
-      id: dok.id,
-      jam1: dok.data().jam1,
-      jam1: dok.data().jam1,
-      x1: dok.data().x1,
-      xi1: dok.data().xi1,
-      xii1: dok.data().xii1,
-      jam2: dok.data().jam2,
-      x2: dok.data().x2,
-      xi2: dok.data().xi2,
-      xii2: dok.data().xii2,
-      jam3: dok.data().jam3,
-      x3: dok.data().x3,
-      xi3: dok.data().xi3,
-      xii3: dok.data().xii3,
-      jam4: dok.data().jam4,
-      x4: dok.data().x4,
-      xi4: dok.data().xi4,
-      xii4: dok.data().xii4
-    });
-  });
-  return hasil;
+  return await docSnap.data();
 }
 
-// ambil data mapel hari jumat
-export async function ambilDaftarJumat() {
-  const refDokumen = collection(db, "jumat");
-  const kueri = query(refDokumen, orderBy("jam1"));
-  const cuplikanKueri = await getDocs(kueri);
-
-  let hasil = [];
-  cuplikanKueri.forEach((dok) => {
-    hasil.push({
-      id: dok.id,
-      jam1: dok.data().jam1,
-      x1: dok.data().x1,
-      xi1: dok.data().xi1,
-      xii1: dok.data().xii1,
-      jam2: dok.data().jam2,
-      x2: dok.data().x2,
-      xi2: dok.data().xi2,
-      xii2: dok.data().xii2,
-      jam3: dok.data().jam3,
-      x3: dok.data().x3,
-      xi3: dok.data().xi3,
-      xii3: dok.data().xii3
-    });
-  });
-  return hasil;
+export function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 // ambil data untuk guru
@@ -195,34 +111,3 @@ export async function ambilDaftarGuru() {
   return hasil;
 }
 
-export function formatAngka(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-}
-
-export async function ubahSenin(docId, jam1, x1, xi1,  xii1,  
-                                       jam2, x2, xi2,  xii2,   
-                                       jam3, x3, xi3, xii3
-) {
-  await updateDoc(doc(db, "senin", docId), {
-    jam1: jam1,
-    x1: x1,
-    xi1: xi1,
-    xii1: xii1,
-    jam2: jam2,
-    x2: x2,
-    xi2: xi2,
-    xii2: xii2,
-    jam3: jam3,
-    x3: x3,
-    xi3: xi3,
-    xii3: xii3
-  });
-}
-
-//  ambil data
-export async function ambilSenin(docId) {
-  const docRef = await doc(db, "senin", docId);
-  const docSnap = await getDoc(docRef);
-
-  return await docSnap.data();
-}
